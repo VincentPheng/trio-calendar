0a1e46907a3ade81bf66917a74eb9dd71ea73389
VincentPheng <vpheng@calpoly.edu>
2022-11-06 10:18:51 -0800

    Calendar View week movement

--- src/pages/CalendarView.tsx
+++ src/pages/CalendarView.tsx
@@ -1,60 +1,111 @@
-import { Button, Flex, Heading, HStack, Text, VStack } from '@chakra-ui/react';
+import {
+  Button,
+  Flex,
+  Heading,
+  HStack,
+  IconButton,
+  Text,
+  VStack,
+} from '@chakra-ui/react';
 import React, { useState } from 'react';
 import { useNavigate, useSearchParams, useParams } from 'react-router-dom';
 import { DateTime as dt } from 'luxon';
 import { TIME_SLOTS } from '../constants';
+import { AiOutlineArrowRight, AiOutlineArrowLeft } from 'react-icons/ai';
 
-type Props = {};
+const URL_PREFIX = (id: string | undefined) => `/meeting/${id ? id : '0'}`;
 
-const URL_PREFIX = (id: string | undefined) =>
-  `/meeting/${id ? id : '0'}/confirm?`;
-
-const CalendarView = (props: Props) => {
+const CalendarView = () => {
   const [qParams] = useSearchParams();
   const { id } = useParams();
   const length = qParams.get('length');
-  const weekNum = dt.now().weekNumber;
-  const date = dt.fromObject({
-    weekYear: 2022,
-    weekNumber: weekNum,
-  });
-  const start = date.startOf('week');
+  const startWeek = sessionStorage.getItem('start');
+
+  const currentDay = dt.now();
+  const start = currentDay.startOf('week');
+  const lastWeek = start.plus({ month: 1 });
+  const currWeekStart = startWeek ? dt.fromISO(JSON.parse(startWeek)) : start;
   const [week, setWeek] = useState([
-    start,
-    start.plus({ days: 1 }),
-    start.plus({ days: 2 }),
-    start.plus({ days: 3 }),
-    start.plus({ days: 4 }),
+    currWeekStart,
+    currWeekStart.plus({ days: 1 }),
+    currWeekStart.plus({ days: 2 }),
+    currWeekStart.plus({ days: 3 }),
+    currWeekStart.plus({ days: 4 }),
   ]);
   const apptTimeSlots = TIME_SLOTS[id ? id : 'snyikos'];
   const navigate = useNavigate();
 
+  const moveWeekForward = () => {
+    const localWeeks = [
+      week[0].plus({ days: 7 }),
+      week[1].plus({ days: 7 }),
+      week[2].plus({ days: 7 }),
+      week[3].plus({ days: 7 }),
+      week[4].plus({ days: 7 }),
+    ];
+    setWeek(localWeeks);
+    sessionStorage.setItem('start', JSON.stringify(localWeeks[0]));
+  };
+
+  const moveWeekBackward = () => {
+    const localWeeks = [
+      week[0].minus({ days: 7 }),
+      week[1].minus({ days: 7 }),
+      week[2].minus({ days: 7 }),
+      week[3].minus({ days: 7 }),
+      week[4].minus({ days: 7 }),
+    ];
+    setWeek(localWeeks);
+    sessionStorage.setItem('start', JSON.stringify(localWeeks[0]));
+  };
   return (
     <Flex h="100%" flexDir="column" justifyContent="center" alignItems="center">
       <Heading fontSize="6xl" mb={6}>
         {length}-minute meeting
       </Heading>
-      <HStack spacing={10}>
+      <HStack w="100%" spacing={10}>
+        <IconButton
+          isDisabled={week[0].toMillis() === start.toMillis()}
+          onClick={moveWeekBackward}
+          alignSelf="start"
+          aria-label="Next Week"
+          icon={<AiOutlineArrowLeft />}
+        />
         {week.map((day) => {
           return (
-            <VStack spacing={4} h="100%">
+            <VStack w="100%" spacing={4} h="100%">
               <Text>{day.toLocaleString(dt.DATE_MED_WITH_WEEKDAY)}</Text>
               {apptTimeSlots[day.weekday].map((timeSlot) => {
-                const dayTime = day.plus(timeSlot);
-                const urlParams = `length=${length}&date=${dayTime}`;
-                return (
-                  <Button
-                    colorScheme="green"
-                    w="100%"
-                    onClick={() => navigate(URL_PREFIX(id) + urlParams)}
-                  >
-                    {dayTime.toLocaleString(dt.TIME_SIMPLE)}
-                  </Button>
-                );
+                if (length) {
+                  const dayTime = day.plus(timeSlot);
+                  const urlParams = `/confirm?length=${length}&date=${dayTime}`;
+                  return (
+                    <Button
+                      isDisabled={
+                        currentDay > dayTime ||
+                        dayTime.plus({ minutes: Number(length) }) >
+                          day.plus({ hour: 17 })
+                      }
+                      colorScheme="green"
+                      w="100%"
+                      onClick={() => navigate(URL_PREFIX(id) + urlParams)}
+                    >
+                      {dayTime.toLocaleString(dt.TIME_SIMPLE)}
+                    </Button>
+                  );
+                }
+                return <Text>ERR</Text>;
               })}
             </VStack>
           );
         })}
+        <IconButton
+          isDisabled={week[4].toMillis() >= lastWeek.toMillis()}
+          onClick={moveWeekForward}
+          alignSelf="start"
+          aria-label="Next Week"
+          icon={<AiOutlineArrowRight />}
+        />
       </HStack>
     </Flex>
   );
--- src/pages/MeetingView.tsx
+++ src/pages/MeetingView.tsx
@@ -1,26 +1,38 @@
 import {
   Avatar,
+  Button,
   Divider,
   GridItem,
+  Icon,
   SimpleGrid,
   Spacer,
   Text,
 } from '@chakra-ui/react';
 import React from 'react';
-import { Outlet, useParams } from 'react-router-dom';
+import { Outlet, useParams, useNavigate } from 'react-router-dom';
 import Card from '../components/Card';
 import { APPOINTMENTS } from '../constants';
+import { AiOutlineArrowLeft } from 'react-icons/ai';
 
 type Props = {};
 
 const MeetingView = (props: Props) => {
   const { id } = useParams();
   const appointment = APPOINTMENTS[id ? id : '0'];
+  const navigate = useNavigate();
 
   return (
     <SimpleGrid columns={10} spacing={3} h="100%">
       <GridItem rowSpan={1} colSpan={2} h="100%">
         <Card h="100%">
+          <Button
+            leftIcon={<Icon mt={1} as={AiOutlineArrowLeft} />}
+            w="100%"
+            mb={5}
+            onClick={() => navigate(-1)}
+          >
+            Go back
+          </Button>
           <Avatar
             bg="gray.300"
             size="2xl"
