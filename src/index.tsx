c4d91e00ce32d4d78d3938d11d717b0ecf6e9cc7
VincentPheng <vpheng@calpoly.edu>
2022-11-05 22:57:21 -0700

    Hashrouter

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,7 @@
 import { ColorModeScript } from '@chakra-ui/react';
 import * as React from 'react';
 import * as ReactDOM from 'react-dom/client';
-import { BrowserRouter as Router } from 'react-router-dom';
+import { HashRouter } from 'react-router-dom';
 import { App } from './App';
 
 const container = document.getElementById('root');
@@ -9,10 +9,10 @@ if (!container) throw new Error('Failed to find the root element');
 const root = ReactDOM.createRoot(container);
 
 root.render(
-  <Router basename={process.env.PUBLIC_URL}>
+  <HashRouter basename={process.env.PUBLIC_URL}>
     <React.StrictMode>
       <ColorModeScript />
       <App />
     </React.StrictMode>
-  </Router>
+  </HashRouter>
 );
