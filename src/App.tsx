c282de301693e8d4768b98c904a2503731bce086
VincentPheng <vpheng@calpoly.edu>
2022-11-05 22:40:41 -0700

    GH Pages route fix

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,6 @@
 import * as React from 'react';
 import { ChakraProvider, Box, theme } from '@chakra-ui/react';
-import { BrowserRouter as Router } from 'react-router-dom';
-import { Route, Routes } from 'react-router';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Landing from './pages/Landing';
 import MeetingView from './pages/MeetingView';
 import TimeSlotView from './pages/TimeSlotView';
--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,7 @@
 import { ColorModeScript } from '@chakra-ui/react';
 import * as React from 'react';
 import * as ReactDOM from 'react-dom/client';
+import { BrowserRouter as Router } from 'react-router-dom';
 import { App } from './App';
 
 const container = document.getElementById('root');
@@ -8,8 +9,10 @@ if (!container) throw new Error('Failed to find the root element');
 const root = ReactDOM.createRoot(container);
 
 root.render(
-  <React.StrictMode>
-    <ColorModeScript />
-    <App />
-  </React.StrictMode>
+  <Router basename={process.env.PUBLIC_URL}>
+    <React.StrictMode>
+      <ColorModeScript />
+      <App />
+    </React.StrictMode>
+  </Router>
 );
