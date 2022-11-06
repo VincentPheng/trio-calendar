fd6d8356d3e8778d705141fc1e098e1389ca8daa
VincentPheng <vpheng@calpoly.edu>
2022-11-05 23:03:54 -0700

    Pages fix

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import * as React from 'react';
 import { ChakraProvider, Box, theme } from '@chakra-ui/react';
-import { Route, Routes } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Landing from './pages/Landing';
 import MeetingView from './pages/MeetingView';
 import TimeSlotView from './pages/TimeSlotView';
@@ -10,14 +10,16 @@ import MeetingInfo from './pages/MeetingInfo';
 export const App = () => (
   <ChakraProvider theme={theme}>
     <Box textAlign="center" fontSize="xl" h="100vh">
-      <Routes>
-        <Route path="/" element={<Landing />} />
-        <Route path="/meeting/:id" element={<MeetingView />}>
-          <Route index element={<TimeSlotView />} />
-          <Route path="slots" element={<CalendarView />} />
-          <Route path="confirm" element={<MeetingInfo />} />
-        </Route>
-      </Routes>
+      <Router basename={`/${process.env.PUBLIC_URL}`} >
+        <Routes>
+          <Route path="" element={<Landing />} />
+          <Route path="/meeting/:id" element={<MeetingView />}>
+            <Route index element={<TimeSlotView />} />
+            <Route path="slots" element={<CalendarView />} />
+            <Route path="confirm" element={<MeetingInfo />} />
+          </Route>
+        </Routes>
+      </Router>
     </Box>
   </ChakraProvider>
 );
--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,6 @@
 import { ColorModeScript } from '@chakra-ui/react';
 import * as React from 'react';
 import * as ReactDOM from 'react-dom/client';
-import { HashRouter } from 'react-router-dom';
 import { App } from './App';
 
 const container = document.getElementById('root');
@@ -9,10 +8,8 @@ if (!container) throw new Error('Failed to find the root element');
 const root = ReactDOM.createRoot(container);
 
 root.render(
-  <HashRouter basename={process.env.PUBLIC_URL}>
-    <React.StrictMode>
-      <ColorModeScript />
-      <App />
-    </React.StrictMode>
-  </HashRouter>
+  <React.StrictMode>
+    <ColorModeScript />
+    <App />
+  </React.StrictMode>
 );
